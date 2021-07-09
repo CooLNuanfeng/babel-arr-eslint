345ce4f9134ea028cf747ec8e40d191b617fa23b
wuhongsong <wuhongsong@haier.com>
2021-07-09 17:47:12 +0800

    arr && arr

--- test/index.js
+++ test/index.js
@@ -1,3 +1,8 @@
 
+var name = arr[0].age
 
-console.log(arr[0].age && arr[0].name && list[0].product && prud[0].name)
+// person.name = arr[0].name
+// arr[0].name = 'blue'
+// arr[0] && arr[0].name && arr[1].age  && list[0].name 
+// person.name = 23
+// person.name && person.job.name && person.job.salary && obj.address.name
